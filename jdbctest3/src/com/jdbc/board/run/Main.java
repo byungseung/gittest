b9bd4d6b116e6bed99fddb3488be4ce13ee3fc45
GOODEE <GOODEE@DESKTOP-T9IHT00>
2023-04-20 15:07:40 +0900

    02_bs main에 출력문 추가

--- jdbctest3/src/com/jdbc/board/run/Main.java
+++ jdbctest3/src/com/jdbc/board/run/Main.java
@@ -5,5 +5,6 @@ import com.jdbc.board.controller.BoardController;
 public class Main {
 	public static void main(String[] args) {
 		new BoardController().mainMenu();
+		System.out.println("내가 추가한것");
 	}
 }
